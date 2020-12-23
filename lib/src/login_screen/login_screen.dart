3aa90c83b685833f0af4eead5915150bf5313338
AMCCG <amornchanchaigul@gmail.com>
2020-12-23 23:07:46 +0700

    UI login screen

--- lib/src/login_screen/login_screen.dart
+++ lib/src/login_screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/svg.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -21,36 +22,85 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Center(
+            Container(
               child: Column(
                 children: <Widget>[
                   Container(
-                    margin: EdgeInsets.only(bottom: 20),
+                    margin: EdgeInsets.only(bottom: 15),
                     child: SvgPicture.asset(
                       'assets/images/svg/chemistry.svg',
-                      width: 300,
-                      height: 300,
+                      width: 250,
+                      height: 250,
                       fit: BoxFit.contain,
                     ),
                   ),
-                  Text('Select an account to sign in with', style: textStyle02)
+                  Text('Select an account to sign in with', style: textStyle02),
+                  Container(
+                    margin: EdgeInsets.only(top: 50),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Row(
+                              children: <Widget>[
+                                Container(
+                                  margin: EdgeInsets.only(right: 15),
+                                  width: 50,
+                                  height: 50,
+                                  child: Center(
+                                      child: SvgPicture.asset(
+                                    'assets/images/svg/user.svg',
+                                    color: Colors.white,
+                                    width: 20,
+                                    height: 20,
+                                  )),
+                                  decoration: BoxDecoration(
+                                      color: Color(0x401875F0),
+                                      shape: BoxShape.circle),
+                                ),
+                                Text('tee_api@hotmail.com', style: textStyle02),
+                              ],
+                            )),
+                        Container(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Row(
+                              children: <Widget>[
+                                Container(
+                                  margin: EdgeInsets.only(right: 15),
+                                  width: 50,
+                                  height: 50,
+                                  child: Center(
+                                      child: SvgPicture.asset(
+                                    'assets/images/svg/user.svg',
+                                    color: Colors.white,
+                                    width: 20,
+                                    height: 20,
+                                  )),
+                                  decoration: BoxDecoration(
+                                      color: Color(0x401875F0),
+                                      shape: BoxShape.circle),
+                                ),
+                                Text('apisit.a@avlgb.com', style: textStyle02),
+                              ],
+                            )),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-            Placeholder(
-              fallbackHeight: 50,
-            ),
-            Center(
+            Container(
               child: Column(
                 children: <Widget>[
                   Container(
-                      margin: EdgeInsets.only(bottom: 20),
+                      margin: EdgeInsets.only(bottom: 15),
                       child: Text('Sign in with another account',
                           style: textStyle01)),
                   Container(
                     width: double.infinity,
                     child: FlatButton(
-                      padding: EdgeInsets.all(20),
+                      padding: EdgeInsets.all(15),
                       onPressed: () {},
                       child: Text('Sign up for free', style: textStyle01),
                       shape: RoundedRectangleBorder(
